87ce76b56be9f1900e1fb18c6e51e13ebfe56a8b
Serhii Karskyi <serhiipraktic7@gmail.com>
2025-05-06 21:16:12 +0100

    add task-1

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,12 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  //   const arrContacts = [];
+
+  //   arrContacts.push(await readContacts());
+
+  //   return arrContacts;
+  return await readContacts();
+};
 
 console.log(await getAllContacts());
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,17 @@
-export const removeLastContact = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+export const removeLastContact = async () => {
+  const lastContact = await readContacts();
+
+  if (lastContact === 0) {
+    console.log(`Upps, we dont have contacts`);
+    return;
+  }
+
+  lastContact.pop();
+
+  await writeContacts(lastContact);
+};
 
 removeLastContact();
