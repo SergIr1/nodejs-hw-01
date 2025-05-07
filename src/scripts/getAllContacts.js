03b828d926b25e27f59d9f83ebee460235cc4886
Serhii Karskyi <serhiipraktic7@gmail.com>
2025-05-07 13:40:05 +0100

    fix promies

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,12 +1,28 @@
 import { readContacts } from '../utils/readContacts.js';
 
 export const getAllContacts = async () => {
-  //   const arrContacts = [];
-
-  //   arrContacts.push(await readContacts());
-
-  //   return arrContacts;
   return await readContacts();
 };
 
+// =============================== варіант 1 ===============================
+
 console.log(await getAllContacts());
+
+// =============================== /варіант 1 ===============================
+
+// =============================== варіант 2 ===============================
+// const getArrContacts = async () => {
+//   const arrContacts = await getAllContacts();
+//   console.log(arrContacts);
+// };
+
+// getArrContacts();
+// =============================== /варіант 2 ===============================
+
+// =============================== варіант 3 ===============================
+
+// getAllContacts()
+//   .then(console.log(`Done`))
+//   .catch(console.log(console.error(`It is a mistake`)));
+
+// =============================== /варіант 3 ===============================
